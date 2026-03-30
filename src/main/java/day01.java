551276e7043ca8cb548c64b08d9f8394eb00af00
RahafAlamri <rhfa9024@gmail.com>
2026-03-30 17:07:41 +0300

    First commit

---
+++ src/main/java/day01.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class day01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello, welcome to our park");
+
+        System.out.println("Please enter your full name: ");
+        String name = input.nextLine();
+        System.out.println("Welcome " + name + ", Good to see you here");
+
+        System.out.println("Please enter your age: ");
+        int age = input.nextInt();
+
+        System.out.println("Please enter your height: ");
+        double height = input.nextDouble();
+
+        System.out.println("Are you a Tuwaiq student? true or false");
+        String isTuwaiqStudent = input.next();
+
+        System.out.println("Please enter your blood type: ");
+        String bloodType = input.next().toUpperCase();
+
+
+        if ((age > 20) && (height > 160)) {
+            System.out.println(name.split(" ")[0] + ", You are eligible for entering the park");
+        } else {
+            System.out.println(name.split(" ")[0] + ", You are not eligible for entering the park");
+        }
+
+        if (isTuwaiqStudent.equalsIgnoreCase( "true")) {
+            System.out.println(name.split(" ")[0] + ", You are eligible for discount");
+        } else {
+            System.out.println(name.split(" ")[0] + ", You are not eligible for discount");
+        }
+
+    }
+}
